7035ad82b14f3c3bdd634e41d1e7efda82c4c67e
natnael <natnaelmalike@gmail.com>
2024-05-03 01:35:08 +0300

    adding Crud operations for all users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,16 @@
 import express from "express";
-import {User, userValidator} from "../models/user.js";
+import { User, userValidator } from "../models/user.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    const users = await User.find().sort({ fullName: 1 });
+    res.send(users);
+});
+
 router.post("/", async (req, res) => {
-    const {error} = userValidator(req.body);
-    if(error) return res.status(400).send(error.details[0].message)
+    const { error } = userValidator(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const user = new User({
         fullName: req.body.fullName,
         email: req.body.email,
@@ -16,5 +21,31 @@ router.post("/", async (req, res) => {
     await user.save();
     res.send(user);
 });
+router.put("/:id", async (req, res) => {
+    const { error } = userValidator(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: req.body.password,
+            department: req.body.department,
+            phone: req.body.phone,
+        },
+        {
+            new: true,
+        }
+    );
+    if(!user) return res.status(404).send('User not Found!')
+    res.send(user)
 
+});
+
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if(!user) return res.status(404).send('User not Found!')
+    res.send(user)
+
+})
 export default router;
