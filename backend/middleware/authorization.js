731fa5b02f24e7e93bc02ea6613f5d93da3b91dc
natnael <natnaelmalike@gmail.com>
2024-06-05 16:31:42 +0300

    "Updated authorization middleware and AuthContext to handle token and user state"

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 export default (req, res, next) => {
     // const token = req.header('x-auth-token')
     const token = req.cookies.token
-    if(!token) return res.status(401).send('Access Denied Buddy, No token Provided')
+    if(!token) return res.status(401).send('Access Denied!, No token Provided')
         try {
             const decoded = jwt.verify(token, process.env.ACCESS_JWT_PRIVATE_KEY)
             req.user = decoded
--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,23 +1,29 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react";
 
-export const AuthContext  = createContext()
+export const AuthContext = createContext();
 export const authReducer = (state, action) => {
     switch (action.type) {
-        case "LOGIN" :
-            return {user: action.payload}
-        case "LOGOUT" : 
-            return {user: null}
-        default: 
-        return state
+        case "LOGIN":
+            return { user: action.payload };
+        case "LOGOUT":
+            return { user: null };
+        default:
+            return state;
     }
-}
+};
 
-export const AuthContextProvider = ({children}) =>{
-    const [state, dispatch] = useReducer(authReducer, {user: null})
-    console.log("Auth Context",state)
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, { user: null });
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        if (user) {
+            dispatch({ type: "LOGIN", payload: user });
+        }
+    },[]);
+    console.log("Auth Context", state);
     return (
-        <AuthContext.Provider value={{...state, dispatch}}>
+        <AuthContext.Provider value={{ ...state, dispatch }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
