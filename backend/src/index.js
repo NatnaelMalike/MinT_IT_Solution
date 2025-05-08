9e411fc0a5dc9f99821c20e3b9d6ebeaf2cbfcda
Natnael Malike <natnaelmalike@gmail.com>
2025-05-09 01:52:11 +0300

    fix: update CORS origin to production URL for secure access

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mint-isuue-report-system.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
